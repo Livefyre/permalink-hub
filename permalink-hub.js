5e14cfb218a039742c317d24c4f29b89d7fde4c1
Joseph Ross <jross@livefyre.com>
2014-08-13 15:41:17 -0700

    Initial commit

---
+++ permalink-hub.js
@@ -0,0 +1,74 @@
+function PermalinkHub(opts){
+    this.collectionHandlers = {};
+    this.hasPermalinkModal = false;
+
+    window.addEventListener('message', this.onPostMessage.bind(this), false);
+}
+
+PermalinkHub.prototype.onPostMessage = function(event){
+    var msg = null; 
+
+    if(typeof event.data === 'object') 
+        msg = event.data 
+    else {
+        try{ 
+            msg = JSON.parse(event.data)
+        } catch(e){ 
+            return; 
+        }       
+    }
+
+    console.log('hub processes event')
+    if(msg.subject !== 'permalink' || !msg.data || !msg.action) 
+        return;
+
+    if(msg.action === 'post'){
+        if(msg.sender === 'permalink-modal')
+            this.receiveModalRegistration(msg.data);
+        else
+            this.receiveAppRegistration(msg.data);
+    }
+
+    if(msg.action === 'put'){
+        if(msg.sender === 'permalink-modal')
+            this.messageAppToPermalink(msg.data);
+    }      
+};
+
+PermalinkHub.prototype.receiveModalHaveAppPermalink = function(data){
+    this.messageAppToPermalink(data);
+};
+
+PermalinkHub.prototype.receiveModalRegistration = function(data){
+    if(this.hasPermalinkModal) return;
+    this.hasPermalinkModal = true;
+    for(key in this.collectionHandlers)
+        this.messageModalAppInfo(this.collectionHandlers[key]);
+};
+
+PermalinkHub.prototype.receiveAppRegistration = function(data){
+    this.collectionHandlers[data.collectionId] = data;
+    if(this.hasPermalinkModal)
+        this.messageModalAppInfo(data);
+};
+
+PermalinkHub.prototype.messageAppToPermalink = function(data){
+    var msg = {
+        sender: 'permalink',
+        subject: data.name,
+        action: 'put',
+        data: data
+    }
+    window.postMessage(JSON.stringify(msg),'*');
+};
+
+PermalinkHub.prototype.messageModalAppInfo = function(data){
+    console.log('Modale App Info ', data)
+    var msg = {
+        sender: 'permalink',
+        subject: 'permalink-modal',
+        action: 'post',
+        data: data
+    }
+    window.postMessage(JSON.stringify(msg),'*');
+};
---
+++ permalink-modal.js
@@ -0,0 +1,83 @@
+function PermalinkModal(opts){
+    this.$el = opts.el;
+    this.name = opts.name;
+    this.collectionId = opts.collectionId;
+    this.networkId = opts.networkId;
+
+
+    this.render();
+    window.addEventListener('message', this.onPostMessage.bind(this), false);
+    this.sendRegistration();
+}
+
+//If message show button
+PermalinkModal.prototype.onPostMessage = function(event){
+    var msg = null; 
+    var status = this.$el.getElementsByClassName('status')[0];
+    var comms = this.$el.getElementsByClassName('comms')[0];
+    var footer = this.$el.getElementsByClassName('footer')[0];
+    var self = this;
+
+    var toggleStatusTo = function($el, statusClassName){
+        var classNames = ['failure', 'success'];
+        for(className in classNames)
+            $el.classList.remove(className);
+        $el.classList.add(statusClassName);
+    };
+
+    if(typeof event.data === 'object') 
+        msg = event.data 
+    else {
+        try{ 
+            msg = JSON.parse(event.data)
+        } catch(e){ 
+            toggleStatusTo('failure');
+            comms.innerHTML = comms.innerHTML + '<li>Failure</li>';
+            return; 
+        }       
+    }
+
+    if(msg.subject !== 'permalink-modal' || !msg.data || msg.action !== 'post') 
+        return;
+
+    toggleStatusTo(status, 'success');
+    comms.innerHTML = comms.innerHTML + '<li>'+ msg.action +'</li>';
+
+    var button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.textContent = msg.data.name;
+    button.onclick = function(){
+        self.messageHubToPermalink(msg.data);
+    };
+
+
+    footer.appendChild(button);
+};
+
+PermalinkModal.prototype.messageHubToPermalink = function(data){
+        var message = {
+            sender: 'permalink-modal',
+            subject: 'permalink',
+            action: 'put',
+            data: data
+        };
+        window.postMessage(JSON.stringify(message),'*');
+}
+
+PermalinkModal.prototype.sendRegistration = function(){
+    var msg = {
+        sender: 'permalink-modal',
+        subject: 'permalink',
+        action: 'post',
+        data: {}
+    };
+    window.postMessage(JSON.stringify(msg),'*');
+};
+
+PermalinkModal.prototype.render = function(){
+    var template = '<div class="header">Permanlink Modal</div>\
+        <div class="status"></div>\
+        <ul class="comms"></ul>\
+        <div class="footer"></div>';
+    this.$el.innerHTML = template;
+};
